b6daaaa999fd0001f001121cc121ddd58c96cef2
sukhpreet kaur <sukhpreetkaur@sukhpreets-MacBook-Air.local>
2025-01-28 09:46:38 +0530

    Add functions to perform basic math operations and the operate function to call each of them as per need

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Hello World!");
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+// The operate() Function
+function operate(a, op, b) {
+    return op(a, b); // Callback function
+}
+
