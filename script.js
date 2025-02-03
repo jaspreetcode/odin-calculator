595f47ae2d6b3d77c7003f2730c75f42f617105f
sukhpreet kaur <sukhpreetkaur@sukhpreets-MacBook-Air.local>
2025-02-03 09:43:27 +0530

    Add basic functionality to receive the first number, operator and the second number

--- script.js
+++ script.js
@@ -20,6 +20,10 @@ function divide(x, y) {
     return x / y;
 }
 
+function mod(x, y) {
+    return x % y;
+}
+
 // The operate() Function
 function operate(a, op, b) {
     return op(a, b); // Callback function
@@ -27,16 +31,42 @@ function operate(a, op, b) {
 
 // Populate the display with digits through buttons
 const digitButtons = document.querySelectorAll('.buttons .digit');
+const operators = document.querySelectorAll(".buttons .operator");
+const equalsOperator = document.querySelector(".buttons .equals");
 const display = document.querySelector(".calculator .display");
 
-function displayDigit(e) {
+function displayDigits(e) {
     display.textContent += e.target.textContent;
     digits = parseInt(display.textContent);
-    console.log(digits)
+}
+
+function getOperator(e) {
+    if (digits) {
+        firstNumber = digits;
+    }
+    operator = e.target.getAttribute("data-op");
+    display.textContent = "";
+    digits = null;
+    console.log(firstNumber);
+    console.log(operator);
+    console.log(digits);
+}
+
+function performOperation(e) {
+    if (digits) {
+        secondNumber = digits;
+    }
+    console.log(secondNumber);
 }
 
 digitButtons.forEach(button => {
-    button.addEventListener("click", displayDigit);
+    button.addEventListener("click", displayDigits);
 });
+
+operators.forEach(operator => {
+    operator.addEventListener("click", getOperator);
+});
+
+equalsOperator.addEventListener("click", performOperation)
 
 
